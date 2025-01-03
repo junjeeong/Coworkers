863d5d0006b8353bc902d6d8bad837bf283911f1
junjeeong <wn8624@naver.com>
2025-01-03 21:16:30 +0900

    프로젝트 설치

---
+++ src/pages/_document.tsx
@@ -0,0 +1,12 @@
+import { Html, Head, Main } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head />
+      <body className="antialiased">
+        <Main />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>HOME페이지</div>;
+};
+
+export default Home;
