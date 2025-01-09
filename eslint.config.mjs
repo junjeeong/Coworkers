57e33e8d1df8c987ff9ff3bc8b8c120e9ab4901b
wn8624 <wn8624@naver.com>
2025-01-09 23:12:07 +0900

    chore: tailwind.config 세팅중

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  "plugin:prettier/recommended", // Prettier 통합
 ];
 
-export default eslintConfig;
+export default {
+  extends: eslintConfig,
+  rules: {
+    // 추가 규칙 설정 (예: Prettier와의 충돌 방지)
+    "prettier/prettier": ["error"], // Prettier 규칙을 ESLint 에러로 설정
+    // 다른 ESLint 규칙 추가 가능
+  },
+  // 필요에 따라 parserOptions 및 env 설정 추가
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,22 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'brand-primary': "#10B981",
+        'brand-secondary': "#34D399",
+        'brand-tertiary': "#A3E635",
+        'brand-gradient': 'linear-gradient(to right, #10B981, #A3E635)',
       },
+      fontSize: {
+        xs: ['12px', { lineHeight: '14px' }],
+        sm: ['13px', { lineHeight: '16px' }],
+        md: ['14px', { lineHeight: '17px' }],
+        lg: ['16px', { lineHeight: '19px' }],
+        xl: ['20px', { lineHeight: '24px' }],
+        '2lg': ['18px', { lineHeight: '21px' }],
+        '2xl': ['24px', { lineHeight: '28px' }],
+        '3xl': ['32px', { lineHeight: '38px' }],
+        '4xl': ['40px', { lineHeight: '48px' }],
+      }
     },
   },
   plugins: [],
